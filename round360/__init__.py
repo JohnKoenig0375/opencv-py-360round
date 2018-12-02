2e6f34f41533d54e9709c8ca91ffbbb8f29b077b
John Koenig <johnkoenig0375@gmail.com>
2018-12-02 17:40:22 -0500

    Added module round360 (placeholder) that will handle the 360round data

--- round360/__init__.py
+++ round360/__init__.py
@@ -3,9 +3,6 @@
 """
 Title: Round360 Module for OpenCV-Python
 Author: John Koenig
-Project: opencv-py-360round
-Version: 0.1.0
-Date: 02DEC2018
 Purpose: Provide a python wrapper to the OpenH264 package for data from the Samsung 360 Round Camera
 Contact: JohnKoenig0375@gmail.com
 """
---
+++ round360/round360.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Title: Round360 Module for OpenCV-Python
+Author: John Koenig
+Project: opencv-py-360round
+Version: 0.1.0
+Date: 02DEC2018
+Purpose: Provide a python wrapper to the OpenH264 package for data from the Samsung 360 Round Camera
+Contact: JohnKoenig0375@gmail.com
+Description:
+    This module is in the planning phase
+    It is designed to 
+"""
+
+
+#%%
+#Import necessary libraries
+
+
+#%%
+#Define Encoded File Format
+
+#TODO
+# - Define Samsung file id layout (I need this information from Samsung)
+
+
+#%%
+#Define Function LoadRaw360Round()
+### Load a raw 360round data file (*.SLS)
+
+#TODO
+# - Load raw 360round data file
+
+
+#%%
+#Define Function DecodeOpenH264()
+###Create OpenH264 decode/split command for raw 360round data
+
+#TODO
+# - Concatenate a custom command (string) for OpenH264 decode/split
+# - Run custom OpenH264 command
+
+
+#%%
+#Define Function OpenCV_wrap()
+###Wrap decoded/split 360round data in open-cv-pyton object
+
+#TODO 
+# - Point to the directory containing the decoded/split 360round data files
+# - Load 17 video files into OpenCV
+# - Load 1 audio file into [to be determined]
+# - Load 1 distance file into python dictionary
+
+
+
+
+
+
+#%%
+#Additional Wrappers
+#Additional wrapper functions for the decoded/split 360 round data will be added below
+
+
+
+
+
+
