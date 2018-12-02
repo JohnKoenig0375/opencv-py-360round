a127cba3b0a7750a5e5e81283c769a832d2b4481
John Koenig <johnkoenig0375@gmail.com>
2018-12-02 16:53:51 -0500

    Added dependency_links for the OpenH264 package

--- setup.py
+++ setup.py
@@ -160,6 +160,11 @@ def main():
     install_requirements = ["numpy>=%s" % numpy_version,
                             'openh264'                       #Added OpenH264 to install_requirements
                             ]
+    
+    #TODO Test this dependency
+    dependency_urls = ['https://github.com/cisco/openh264/releases/download/v1.8.0/Source.Code.tar.gz.gz'
+                        ]   #Linux (64-bit)   https://github.com/cisco/openh264/releases
+    
     skbuild.setup(
         name=package_name,
         version=package_version,
@@ -173,6 +178,7 @@ def main():
         maintainer="Olli-Pekka Heinisuo",
         include_package_data=True,
         ext_modules=EmptyListWithLength(),
+        dependency_links=dependency_urls,
         install_requires=install_requirements,
         classifiers=[
           'Development Status :: 5 - Production/Stable',
