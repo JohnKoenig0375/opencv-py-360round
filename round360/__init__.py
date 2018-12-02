7a6472774fd74196559606e6d382b7f98d329333
John Koenig <johnkoenig0375@gmail.com>
2018-12-02 17:04:28 -0500

    Added placeholder for round360 module

---
+++ round360/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Title: Round360 Module for OpenCV-Python
+Project: opencv-py-360round
+Version: 0.1.0
+Date: 02DEC2018
+Purpose: Provide a python wrapper to the OpenH264 package
+"""
+
