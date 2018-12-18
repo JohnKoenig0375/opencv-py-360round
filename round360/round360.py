12d0639492139711912238baf5b21a8b23bff0f5
John Koenig <johnkoenig0375@gmail.com>
2018-12-18 10:16:49 -0500

    Add code to "load" function

--- round360/round360.py
+++ round360/round360.py
@@ -16,7 +16,9 @@ Description:
 
 #%%
 #Import necessary libraries
-
+import cv2
+import os
+import re
 
 #%%
 #Define Encoded File Format
@@ -52,9 +54,41 @@ Description:
 # - Load 1 audio file into [to be determined]
 # - Load 1 distance file into python dictionary
 
+split360_dir =  '/media/md33a/360_4tb_external/360_projects/GWU_capstone/lanterns_split360/'
+
+#def OpenCV_wrap(split360_dir):
+    
+#Get file list
+split360_files = os.listdir(split360_dir)
+
+#Get of files in split360_dir
+num_split360_files =len(split360_files)
+
+#If num files not 19, end funtion
+if len(split360_files) != 19:
+    print('num_split360_files in split360_dir = %s' % (str(num_split360_files)))
+    print('Total number of files must == 19')
+    print('Make sure all 360round split files are present in the split360_dir and no other files')
+    #return files
+
+txt_filename = [file for file in split360_files if re.match('\S*\.txt',file) is not None][0]
+wav_filename = [file for file in split360_files if re.match('(\S*\.wav)',file) is not None][0]
+video_filenames = [file for file in split360_files if file not in [txt_filename,wav_filename]]
+
+#for v in video_filenames:
+    
 
 
 
+
+'''
+return txt_file_index
+
+
+test = OpenCV_wrap(split360_dir)
+
+print(test)
+'''
 
 
 #%%
